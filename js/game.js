07f9e8d2875fe1c27517e940768256fc296d1599
marco-cuconato <99355087+marco-cuconato@users.noreply.github.com>
2022-04-14 17:21:17 +0000

    add space

--- js/game.js
+++ js/game.js
@@ -1,11 +1,11 @@
-/*golbal Phaser*/
+/*global Phaser*/
 
 const config = {
 type: Phaser.AUTO,
 width: 1920,
 height: 1080,
-backgroundColor: 0x5f6e7a
-}
+backgroundColor: 0x5f6e7a,
+};
 
-const game = new Phaseer.Game(config)
-console.log(game)
+const game = new Phaser.Game(config)
+console.log(game);
