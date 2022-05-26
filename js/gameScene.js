ed33381866e36359984d2975a898c669546ec466
marco-cuconato <99355087+marco-cuconato@users.noreply.github.com>
2022-05-26 16:25:20 +0000

    a thing

--- js/gameScene.js
+++ js/gameScene.js
@@ -82,6 +82,7 @@ class GameScene extends Phaser.Scene {
       shipCollide.destroy()
       this.gameOverText = this.add.text (1920 / 2, 1080 / 2, "Game Over!\nClick to play again.", this.gameOverTextStyle).setOrigin(0.5)
       this.gameOverText.setInteractive({ useHandCursor: true })
+      this.score = 0
       this.gameOverText.on("pointerdown", () => this.scene.start("gameScene"))
     }.bind(this))
   }
